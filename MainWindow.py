e82d704ac359261363ac9794106c544cbc8421a5
mimmo02 <157637052+mimmo02@users.noreply.github.com>
2024-03-14 15:06:37 +0100

    Add files via upload

--- MainWindow.py
+++ MainWindow.py
@@ -8,8 +8,10 @@ import tkinter.font as tkFont
 from View.PlaceButtons import placeButtons
 import View.MainControl as mc
 
-# from Utils import TankSystemProxy as t     // remove comment here  !!!!
-#from Utils import Initialisierung as i      // remove comment here  !!!!
+
+from Utils import TankSystemProxy as t     
+from Utils import Initialization as i     
+
 from ctypes import windll
 windll.shcore.SetProcessDpiAwareness(1)
      
@@ -18,23 +20,25 @@ root = tk.Tk()
 def_font = tkFont.nametofont("TkDefaultFont")
 def_font.config(size=12)
 
-width=700
-height=650
+width=500
+height=500
 xpos = 150
 ypos = 150
 root.geometry(f'{width}x{height}+{xpos}+{ypos}')
 
 #set window title
 root.title('SiTas MainWindow')
+mainControl = mc.MainControl()
+
 
 #init some busines classes
-#tankSystemProxy = t.TankSystemProxy()            // remove comment here  !!!!
+tankSystemProxy = t.TankSystemProxy()            
+components = i.Initialization(tankSystemProxy)    
 
-#components = i.Initialisierung(tankSystemProxy)    // remove comment here  !!!!
-mainControl = mc.MainControl()
-#mainControl.setTankSystemProxy(tankSystemProxy)    // remove comment here  !!!!
-#mainControl.setComponents(components)         // remove comment here  !!!!
-#tankSystemProxy.setComponents(components)    // remove comment here  !!!!
+mainControl.setTanksystemProxy(tankSystemProxy)    
+mainControl.setComponents(components)        
+tankSystemProxy.setComponents(components)    
+
 
 placeButtons(root, mainControl)
 
